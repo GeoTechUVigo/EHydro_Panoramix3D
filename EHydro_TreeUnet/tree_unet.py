c8fbdd8f4701fa23cf6e964637fcc1b78a05d8ee
Samuel Novoa <samuel.novoa@uvigo.gal>
2025-05-20 09:41:10 +0200

    [Feat] Cambios varios para la segmentación de instancia

--- EHydro_TreeUnet/tree_unet.py
+++ EHydro_TreeUnet/tree_unet.py
@@ -1,3 +1,4 @@
+import torch
 import torchsparse
 
 from torch import nn
@@ -50,6 +51,22 @@ class UpSample(nn.Module):
         x = torchsparse.cat([x1, x2])
         
         return self.conv(x)
+
+class UpSampleOffset(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.Sequential(
+            spnn.Conv3d(in_channels, in_channels // 3, kernel_size=2, stride=2, transposed=True, generative=False),
+            spnn.BatchNorm(in_channels // 3),
+            spnn.ReLU(inplace=True)
+        )
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2, x3):
+        x1 = self.up(x1)
+        x = torchsparse.cat([x1, x2, x3])
+        
+        return self.conv(x)
     
 class Encoder(nn.Module):
     def __init__(self, in_channels, base_channels, deep):
@@ -72,29 +89,46 @@ class Encoder(nn.Module):
         b = self.bottle_neck(x)
         return b, downs
     
-class Decoder(nn.Module):
-    def __init__(self, num_classes, base_channels, deep):
+class DecoderSemantic(nn.Module):
+    def __init__(self, num_classes, base_channels, depth):
         super().__init__()
-        channels = [base_channels * (2 ** i) for i in range(deep, -1, -1)]
+        channels = [base_channels * (2 ** i) for i in range(depth, -1, -1)]
         
-        self.up_convolutions = nn.ModuleList([UpSample(channels[i], channels[i + 1]) for i in range(deep)])
+        self.up_convolutions = nn.ModuleList([UpSample(channels[i], channels[i + 1]) for i in range(depth)])
         self.out = spnn.Conv3d(in_channels=channels[-1], out_channels=num_classes, kernel_size=1, padding=0, stride=1)
 
     def forward(self, b, downs):
+        ups = []
+        for i, up_convolution in enumerate(self.up_convolutions):
+            b = up_convolution(b, downs[i])
+            ups.append(b)
+
+        return self.out(b), ups
+    
+class DecoderOffset(nn.Module):
+    def __init__(self, base_channels, depth):
+        super().__init__()
+        channels = [base_channels * (2 ** i) for i in range(depth, -1, -1)]
+        
+        self.up_convolutions = nn.ModuleList([UpSample(channels[i], channels[i + 1]) for i in range(depth)])
+        self.out = spnn.Conv3d(in_channels=channels[-1], out_channels=3, kernel_size=1, padding=0, stride=1)
+
+    def forward(self, b, downs, ups):
         for i, up_convolution in enumerate(self.up_convolutions):
             b = up_convolution(b, downs[i])
 
         return self.out(b)
-
-
+    
 class UNet(nn.Module):
-    def __init__(self, in_channels, num_classes, base_channels=64, deep=4):
+    def __init__(self, in_channels, num_classes, base_channels=64, depth=4):
         super().__init__()
-        self.encoder = Encoder(in_channels, base_channels, deep)
-        self.decoder = Decoder(num_classes, base_channels, deep)
+        self.encoder = Encoder(in_channels, base_channels, depth)
+        self.decoder_semantic = DecoderSemantic(num_classes, base_channels, depth)
+        self.decoder_offset = DecoderOffset(base_channels, depth)
 
     def forward(self, x):
         b, downs = self.encoder(x)
-        semantic = self.decoder(b, downs)
+        semantic, ups = self.decoder_semantic(b, downs)
+        offset = self.decoder_offset(b, downs, ups)
 
-        return semantic
+        return semantic, offset
