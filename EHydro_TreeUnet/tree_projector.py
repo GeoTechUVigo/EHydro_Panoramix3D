32156b7af8f3abe41db0d828e119d264eb09a0a3
Samuel Novoa <samuel.novoa@uvigo.gal>
2025-06-09 10:53:44 +0200

    [Feat] Añadido notebook para preprocesar datasets y varios cambios para pruebas

--- EHydro_TreeUnet/tree_projector.py
+++ EHydro_TreeUnet/tree_projector.py
@@ -56,7 +56,7 @@ class PixelDecoder(nn.Module):
         ) for channel in channels)
         
         self.upsample = nn.ModuleList(nn.Sequential(
-            spnn.Conv3d(latent_dim, latent_dim, kernel_size=2, stride=2, transposed=True, generative=False, bias=False),
+            spnn.Conv3d(latent_dim, latent_dim, kernel_size=2, stride=2, transposed=True, bias=False),
             spnn.BatchNorm(latent_dim),
             spnn.ReLU(inplace=True)
         ) for _ in channels)
@@ -68,7 +68,7 @@ class PixelDecoder(nn.Module):
         ) for _ in channels)
 
         self.last_upsample = nn.Sequential(
-            spnn.Conv3d(latent_dim, latent_dim, kernel_size=2, stride=2, transposed=True, generative=False, bias=False),
+            spnn.Conv3d(latent_dim, latent_dim, kernel_size=2, stride=2, transposed=True, bias=False),
             spnn.BatchNorm(latent_dim),
             spnn.ReLU(inplace=True),
 
@@ -92,34 +92,35 @@ class PixelDecoderAlt(nn.Module):
     def __init__(self, in_channels, channels, latent_dim):
         super().__init__()
         self.upsample = nn.ModuleList(nn.Sequential(
-            spnn.Conv3d(sum(channels[i:]), sum(channels[i:]), kernel_size=2, stride=2, transposed=True, generative=False, bias=False),
+            spnn.Conv3d(sum(channels[i:]), sum(channels[i:]), kernel_size=2, stride=2, transposed=True, bias=False),
             spnn.BatchNorm(sum(channels[i:])),
             spnn.ReLU(inplace=True)
         ) for i in range(len(channels)))
 
         self.smooth = nn.Sequential(
             spnn.Conv3d(sum(channels) + in_channels, latent_dim, 3, padding=1, bias=False),
-            spnn.BatchNorm(sum(channels) + in_channels),
+            spnn.BatchNorm(latent_dim),
+            spnn.ReLU(inplace=True),
+            spnn.Conv3d(latent_dim, latent_dim, 3, padding=1, bias=False),
+            spnn.BatchNorm(latent_dim),
             spnn.ReLU(inplace=True)
         )
     
     def forward(self, x):
-        result = x[-1]
         for i in range(len(x)-2, -1, -1):
-            result = self.upsample[i](result)
-            new_F = cat([result.F, x[i].F], dim=1)
-            result = SparseTensor(coords=result.C, feats=new_F)
+            x[-1] = self.upsample[i](x[-1])
+            x[-1].F = cat([x[-1].F, x[i].F], dim=1)
 
-        return self.smooth(result)
+        return self.smooth(x[-1])
     
 class TreeProjector(nn.Module):
-    def __init__(self, in_channels, num_classes, max_instances, channels = [64, 128, 256, 512], latent_dim = 512):
+    def __init__(self, in_channels, num_classes, max_instances, channels = [64, 128, 256], latent_dim = 512):
         super().__init__()
         self.encoder = Encoder(in_channels, channels)
         self.pixel_decoder = PixelDecoderAlt(in_channels, channels, latent_dim)
         self.semantic_head = spnn.Conv3d(latent_dim, num_classes, 1, bias=False)
-        self.instance_head = spnn.Conv3d(latent_dim, max_instances, 1, bias=False)
+        # self.instance_head = spnn.Conv3d(latent_dim, max_instances, 1, bias=False)
 
     def forward(self, x):
         feats = self.pixel_decoder(self.encoder(x))
-        return self.semantic_head(feats), self.instance_head(feats)
+        return self.semantic_head(feats) #, self.instance_head(feats)
