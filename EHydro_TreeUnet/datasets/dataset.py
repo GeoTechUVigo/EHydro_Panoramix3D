86bf65ab85ef0f8fac6de411e8926c67c4983e52
Samuel Novoa <samuel.novoa@uvigo.gal>
2025-06-26 18:29:29 +0200

    [Fix] Archivos faltantes

---
+++ EHydro_TreeUnet/datasets/dataset.py
@@ -0,0 +1,116 @@
+import torch
+import numpy as np
+
+import laspy
+from plyfile import PlyData
+
+from torchsparse import SparseTensor
+from torchsparse.utils.quantize import sparse_quantize
+
+
+class Dataset:
+    def __init__(self, files, voxel_size: float, data_augmentation: float = 1.0, yaw_range = (0, 360), tilt_range = (-5, 5), scale = (0.9, 1.1), feat_keys=['intensity', 'x_norm', 'y_norm', 'z_norm', 'wavelength']) -> None:
+        self._rng = np.random.default_rng()
+        self._files = files
+        self._feat_keys = feat_keys
+
+        self._voxel_size = voxel_size
+        self._len = int(len(self._files) * data_augmentation)
+        
+        self._yaw_range = yaw_range
+        self._tilt_range = tilt_range
+        self._scale = scale
+        
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            return [self._preprocess(i) for i in range(*idx.indices(len(self)))]
+        elif isinstance(idx, int):
+            if idx < 0:
+                idx += len(self)
+            if idx < 0 or idx >= len(self):
+                raise IndexError("Index out of range")
+            return self._preprocess(idx)
+        else:
+            raise TypeError("Index must be a slice or an integer")
+        
+    def __len__(self):
+        return self._len
+    
+    def _load_file(self, path):
+        ext = path.suffix.lower()
+
+        coords = ...
+        feats = ...
+        semantic_labels = ...
+        
+        if ext in ('.las, .laz'):
+            file = laspy.read(path)
+
+            coords = np.vstack((file.x, file.y, file.z)).transpose()
+            intensity = np.array(file.I_norm)[:, None]
+
+            coords_min = coords.min(axis=0)
+            coords_norm = (coords - coords_min) / (coords.max(axis=0) - coords_min)
+
+            wavelength = np.array(file.wavelength)[:, None]
+
+            select_columns = []
+            if 'intensity' in self._feat_keys:
+                select_columns.append(intensity)
+            if 'x_norm' in self._feat_keys:
+                select_columns.append(coords_norm[:, [0]])
+            if 'y_norm' in self._feat_keys:
+                select_columns.append(coords_norm[:, [1]])
+            if 'z_norm' in self._feat_keys:
+                select_columns.append(coords_norm[:, [2]])
+            if 'wavelength' in self._feat_keys:
+                select_columns.append(wavelength)
+
+            feats = np.hstack(select_columns)
+
+            semantic_labels = np.array(file.classification)
+            instance_labels = np.array(file.treeID)
+        else:
+            raise ValueError(f'Unsopported file extension: {ext}!')
+
+        return coords, feats, semantic_labels, instance_labels
+    
+    def _agument_data(self, coords):
+        yaw = np.deg2rad(self._rng.uniform(*self._yaw_range))
+        pitch = np.deg2rad(self._rng.uniform(*self._tilt_range))
+        roll = np.deg2rad(self._rng.uniform(*self._tilt_range))
+        scale = self._rng.uniform(*self._scale)
+
+        cy, sy = np.cos(yaw), np.sin(yaw)
+        cp, sp = np.cos(pitch), np.sin(pitch)
+        cr, sr = np.cos(roll), np.sin(roll)
+
+        rotation_mtx = np.array([[cy*cp,  cy*sp*sr - sy*cr,  cy*sp*cr + sy*sr],
+                                 [sy*cp,  sy*sp*sr + cy*cr,  sy*sp*cr - cy*sr],
+                                 [ -sp ,            cp*sr ,            cp*cr ]],
+                                dtype=coords.dtype)
+
+        return (coords @ rotation_mtx.T) * scale
+        
+    def _preprocess(self, idx: int):
+        coords, feat, semantic_labels, instance_labels = self._load_file(self._files[idx % len(self._files)])
+        if idx >= len(self._files):
+            coords = self._agument_data(coords)
+
+        coords -= np.min(coords, axis=0, keepdims=True)
+
+        voxels, indices, inverse_map = sparse_quantize(coords, self._voxel_size, return_index=True, return_inverse=True)
+        feat = feat[indices]
+        semantic_labels = semantic_labels[indices]
+        instance_labels = instance_labels[indices]
+
+        voxels = torch.tensor(voxels, dtype=torch.int)
+        feat = torch.tensor(feat.astype(np.float32), dtype=torch.float)
+        semantic_labels = torch.tensor(semantic_labels, dtype=torch.long)
+        instance_labels = torch.tensor(instance_labels, dtype=torch.long)
+
+        inputs = SparseTensor(coords=voxels, feats=feat)
+        semantic_labels = SparseTensor(coords=voxels, feats=semantic_labels)
+        instance_labels = SparseTensor(coords=voxels, feats=instance_labels)
+
+        return {"inputs": inputs, "semantic_labels": semantic_labels, "instance_labels": instance_labels, "coords": coords, "inverse_map": inverse_map}
---
+++ EHydro_TreeUnet/datasets/mixed_dataset.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+from .dataset import Dataset
+from pathlib import Path
+
+
+class MixedDataset:
+    def __init__(self, voxel_size: float = 0.2, train_pct: float = 0.8, data_augmentation: float = 1.0, yaw_range = (0, 360), tilt_range = (-5, 5), scale = (0.9, 1.1), feat_keys=['intensity', 'x_norm', 'y_norm', 'z_norm', 'wavelength']) -> None:
+        self._folder = Path('./datasets/MixedDataset')
+        self._extensions = ('.laz', '.las')
+
+        self._feat_channels = len(feat_keys)
+        self._num_classes = 4
+        self._class_names = ['Terrain', 'Low Vegetation', 'Stem', 'Canopy']
+        self._class_colormap = np.array([
+            [128, 128, 128], # clase 0 - Terrain - gris
+            [147, 255, 138], # clase 1 - Low vegetation - verde claro
+            [255, 165, 0],   # clase 2 - Stem - naranja
+            [0, 128, 0],     # clase 3 - Canopy - verde oscuro
+        ], dtype=np.uint8)
+
+        files = sorted(
+            [f for f in self._folder.rglob("*") if f.is_file() and f.suffix.lower() in self._extensions],
+            key=lambda f: f.name
+        )
+
+        train_idx = int(train_pct * len(files))
+        self._train_dataset = Dataset(files[:train_idx], voxel_size=voxel_size, data_augmentation=data_augmentation, yaw_range=yaw_range, tilt_range=tilt_range, scale=scale, feat_keys=feat_keys)
+        self._val_dataset = Dataset(files[train_idx:], voxel_size=voxel_size, feat_keys=feat_keys)
+
+    @property
+    def feat_channels(self):
+        return self._feat_channels
+    
+    @property
+    def num_classes(self):
+        return self._num_classes
+
+    @property
+    def class_names(self):
+        return self._class_names
+    
+    @property
+    def class_colormap(self):
+        return self._class_colormap
+    
+    @property
+    def train_dataset(self):
+        return self._train_dataset
+    
+    @property
+    def val_dataset(self):
+        return self._val_dataset
+    
