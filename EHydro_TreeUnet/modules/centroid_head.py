c13c27742154f9ae6426f7a0f51354daed507cab
Samuel Novoa <samuel.novoa@uvigo.gal>
2025-07-24 13:47:33 +0200

    [Fix] Pequeños cambios faltantes

--- EHydro_TreeUnet/modules/centroid_head.py
+++ EHydro_TreeUnet/modules/centroid_head.py
@@ -15,7 +15,8 @@ class CentroidHead(nn.Module):
         nn.init.constant_(self.conv.bias, bias_val = torch.log(instance_density / (1 - instance_density)))
         nn.init.kaiming_normal_(self.conv.weight, mode='fan_out')
 
-    def _find_centroid_peaks(self, centroid_scores, tau=0.1, radius=1.1):
+    @torch.no_grad()
+    def _find_centroid_peaks(self, feats, centroid_scores, tau=0.1, radius=1.1):
         logits  = centroid_scores.F.squeeze(-1)
         heat    = torch.sigmoid(logits)
 
@@ -24,7 +25,8 @@ class CentroidHead(nn.Module):
             return centroid_scores.C.new_zeros((0, 4))
         
         cand_h = heat[mask]
-        cand_c = centroid_scores.C[mask]
+        cand_c = feats.C[mask]
+        cand_f = feats.F[mask]
 
         edge_index = radius_graph(
             cand_c[:, 1:].float(), radius,
@@ -33,12 +35,29 @@ class CentroidHead(nn.Module):
 
         maxi, _ = scatter_max(cand_h[edge_index[1]], edge_index[0], dim_size=cand_h.size(0))
         is_peak = cand_h >= maxi
+        peaks = cand_c[is_peak]
 
-        peaks = cand_c[is_peak]                    # (K,4)
+        if peaks.shape[0] == 0:
+            return peaks, feats.F.new_zeros((0, cand_f.size(1)))
+        
+        edge_pc = radius_graph(
+            x=cand_c[:,1:].float(),        # puntos
+            pos=peaks[:,1:].float(),       # picos (query set)
+            r=r_cluster,
+            x_batch=cand_c[:,0], pos_batch=peaks[:,0],
+            max_num_neighbors=100
+        )
+        cand_idx = edge_pc[0]             # índices en cand_c / cand_f
+        peak_idx = edge_pc[1]             # índices en peaks
+
+        # --------- 4. media de features por pico ------------------
+        feat_mean = scatter_mean(cand_f[cand_idx], peak_idx,
+                                dim=0, dim_size=peaks.size(0))
+
         return peaks
 
     def forward(self, x):
         centroid_scores = self.conv(x)
-        centroid_peaks = self._find_centroid_peaks(centroid_scores)
+        centroid_peaks, centroid_feats = self._find_centroid_peaks(centroid_scores)
 
         return centroid_scores, SparseTensor(coords=centroid_peaks, feats=centroid_feats)
